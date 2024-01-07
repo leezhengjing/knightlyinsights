79acbdfd2625b98cbef3b19a26262b31e99cfafd
leezhengjing <zhengjing.lee86@gmail.com>
2024-01-07 12:55:27 +0800

    Fix pocketbase autocancellation

--- knightly-insights/src/app/utils/pocketbase.tsx
+++ knightly-insights/src/app/utils/pocketbase.tsx
@@ -1,4 +1,7 @@
 import PocketBase from 'pocketbase';
 
 const url = 'https://knightly-insights.pockethost.io/'
-export const pb = new PocketBase(url)
+const pb = new PocketBase(url)
+pb.autoCancellation(false)
+
+export { pb }
